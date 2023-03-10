772023b4b19c110d416f6bf7e5fcc2f690e86c35
VendhanRaga <aeroragavendra@gmail.com>
2023-03-10 06:28:59 +0530

    commit_first_commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Details
+      tittle="FREE"
+      duration="0"
+      access="✓"
+      user="single User"
+      storage="5GB Storage"
+      projects="Unlimited Public Projects"
+      community="community Access"
+      denied="✕"
+      private="Unlimited Private Projects"
+      support="Dedicated Phone Support"
+      subdomain="Free Subdomain "
+      status="Monthly Status Reports"
+       />
+
+<Details
+      tittle="PLUS"
+      duration="9"
+      access="✓"
+      user="single User"
+      storage="5GB Storage"
+      projects="Unlimited Public Projects"
+      community="community Access"
+      denied="✓"
+      private="Unlimited Private Projects"
+      support="Dedicated Phone Support"
+      subdomain="Free Subdomain "
+      status="❌Monthly Status Reports"
+       />
+
+<Details
+      tittle="PRO"
+      duration="49"
+      access="✓"
+      user="single User"
+      storage="5GB Storage"
+      projects="Unlimited Public Projects"
+      community="community Access"
+      denied="✓"
+      private="Unlimited Private Projects"
+      support="Dedicated Phone Support"
+      subdomain="Free Subdomain "
+      status="Monthly Status Reports"
+       />
+   
     </div>
   );
 }
 
 export default App;
+
+function Details(props){
+  return(
+<div className="card">
+  <h6>
+    <p>{props.tittle}</p>
+  </h6>
+  <h1>
+    <p>$ {props.duration}/mounth</p>
+  </h1>
+  <hr></hr>
+  <h4>
+    <span>{props.access} {props.user}</span>
+  </h4>
+  <h4>
+    <span>{props.access} {props.storage}</span>
+  </h4>
+  <h4>
+    <span>{props.access} {props.projects}</span>
+  </h4>
+  <h4>
+    <span>{props.access} {props.community}</span>
+  </h4>
+  <h4>
+    <span>{props.denied} {props.private}</span>
+  </h4>
+  <h4>
+    <span>{props.denied} {props.support}</span>
+  </h4>
+  <h4>
+    <span>{props.denied} {props.subdomain}</span>
+  </h4>
+  <h4>
+    <span>{props.denied} {props.status}</span>
+  </h4>
+  <button className='btn'>BUTTON</button>
+
+</div>
+  )
+};
